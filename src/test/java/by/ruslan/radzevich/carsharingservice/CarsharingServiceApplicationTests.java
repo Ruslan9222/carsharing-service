50bc0839bd43bf716dc385ab7557c97d43a4423b
Notebook <magog21@gmail.com>
2023-11-30 12:49:18 +0300

    master commit

---
+++ src/test/java/by/ruslan/radzevich/carsharingservice/CarsharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ruslan.radzevich.carsharingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsharingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
