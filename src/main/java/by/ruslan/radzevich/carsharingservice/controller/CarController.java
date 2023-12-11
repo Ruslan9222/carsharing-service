133e9eba4d47f0f2d95ae841e93b03424e671861
Notebook <magog21@gmail.com>
2023-12-12 00:48:31 +0300

    Add_Search_Car_Nearby

--- src/main/java/by/ruslan/radzevich/carsharingservice/controller/CarController.java
+++ src/main/java/by/ruslan/radzevich/carsharingservice/controller/CarController.java
@@ -1,34 +1,34 @@
-//package by.ruslan.radzevich.carsharingservice.controller;
-//
-//import by.ruslan.radzevich.carsharingservice.dto.NewCarDto;
-//import by.ruslan.radzevich.carsharingservice.model.Car;
-//import by.ruslan.radzevich.carsharingservice.repository.CarRepository;
-//import by.ruslan.radzevich.carsharingservice.service.CarService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/cars")
-//public class CarController {
-//    @Autowired
-//    private CarService carService;
-//    @Autowired
-//    private CarRepository carRepository;
-//
-//
-//    @PostMapping("/car")
-////    ResponseEntity<Car> create(@RequestBody NewCarDto newCarDto) {
-//////        Car car = carRepository.save(newCarDto);
-//////        return ResponseEntity.ok(car);
-////    }
-//
-//    @GetMapping("/nearby")
-//    public List<Car> getCarsNearby(@RequestParam double latitude,
-//                                   @RequestParam double longitude,
-//                                   @RequestParam double radius) {
-//        return carService.findCarsNearby(latitude, longitude, radius);
+package by.ruslan.radzevich.carsharingservice.controller;
+
+import by.ruslan.radzevich.carsharingservice.dto.NewCarDto;
+import by.ruslan.radzevich.carsharingservice.model.Car;
+import by.ruslan.radzevich.carsharingservice.repository.CarRepository;
+import by.ruslan.radzevich.carsharingservice.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+    @Autowired
+    private CarService carService;
+    @Autowired
+    private CarRepository carRepository;
+
+
+    @PostMapping("/car")
+//    ResponseEntity<Car> create(@RequestBody NewCarDto newCarDto) {
+////        Car car = carRepository.save(newCarDto);
+////        return ResponseEntity.ok(car);
 //    }
-//}
+
+    @GetMapping("/nearby")
+    public List<Car> getCarsNearby(@RequestParam double latitude,
+                                   @RequestParam double longitude,
+                                   @RequestParam double radius) {
+        return carService.findCarsNearby(latitude, longitude, radius);
+    }
+}
--- src/main/java/by/ruslan/radzevich/carsharingservice/repository/CarRepository.java
+++ src/main/java/by/ruslan/radzevich/carsharingservice/repository/CarRepository.java
@@ -1,10 +1,10 @@
-//package by.ruslan.radzevich.carsharingservice.repository;
-//
-//import by.ruslan.radzevich.carsharingservice.model.Car;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//public interface CarRepository extends JpaRepository <Car, Long> {
-//    List<Car> findByLatitudeBetweenAndLongitudeBetween(double minLat, double maxLat, double minLong, double maxLong);
-//}
+package by.ruslan.radzevich.carsharingservice.repository;
+
+import by.ruslan.radzevich.carsharingservice.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CarRepository extends JpaRepository <Car, Long> {
+    List<Car> findByLatitudeBetweenAndLongitudeBetween(double minLat, double maxLat, double minLong, double maxLong);
+}
