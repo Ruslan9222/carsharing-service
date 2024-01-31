5e921596820fa68f8eaf8583b4ded38b299a8030
Notebook <magog21@gmail.com>
2024-01-31 12:52:32 +0300

    update

--- src/main/java/by/ruslan/radzevich/carsharingservice/repository/CardRepository.java
+++ src/main/java/by/ruslan/radzevich/carsharingservice/repository/CardRepository.java
@@ -4,5 +4,4 @@ import by.ruslan.radzevich.carsharingservice.model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
-    ///
 }
