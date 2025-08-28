84e7a616103ffa56a8fde7848ec978b3a5a840a9
Ruslan9222 <magog21@gmail.com>
2025-08-28 17:29:42 +0300

    add Readme

--- carsharing-domain/src/main/java/by/ruslan/radzevich/model/entity/Car.java
+++ carsharing-domain/src/main/java/by/ruslan/radzevich/model/entity/Car.java
@@ -22,18 +22,5 @@ public class Car extends AbstractModelId {
     private double longitude;
     private byte[] photo;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Car car = (Car) o;
-        return Double.compare(latitude, car.latitude) == 0 && Double.compare(longitude, car.longitude) == 0 && Objects.equals(model, car.model) && Objects.equals(winCode, car.winCode) && Objects.equals(classCar, car.classCar) && Arrays.equals(photo, car.photo);
-    }
 
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(model, winCode, classCar, latitude, longitude);
-        result = 31 * result + Arrays.hashCode(photo);
-        return result;
-    }
 }
--- carsharing-domain/src/main/java/by/ruslan/radzevich/repository/CarRepository.java
+++ carsharing-domain/src/main/java/by/ruslan/radzevich/repository/CarRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CarRepository extends JpaRepository<by.ruslan.radzevich.model.entity.Car, Long> {
+
     List<CarsView> findAllBy();
-    List<by.ruslan.radzevich.model.entity.Car> findByLatitudeBetweenAndLongitudeBetween(double minLat,
-                                                       double maxLat,
-                                                       double minLong,
-                                                       double maxLong);
+
+    List<by.ruslan.radzevich.model.entity.Car> findByLatitudeBetweenAndLongitudeBetween(
+        double minLat,
+        double maxLat,
+        double minLong,
+        double maxLong);
 }
