b3d0c0f6c2df0ae55ce4519b074672d0e206a58d
Ruslan9222 <magog21@gmail.com>
2025-09-01 16:32:38 +0300

    fix

--- carsharing-domain/src/main/java/by/ruslan/radzevich/repository/CarPriceRepository.java
+++
@@ -1,7 +0,0 @@
-//package by.ruslan.radzevich.repository;
-//
-//import by.ruslan.radzevich.carsharingservice.model.CarPrice;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CarPriceRepository extends JpaRepository<by.ruslan.radzevich.model.entity.CarPrice,Long> {
-//}
